472f785121e13df9765b43c09f988a99d224ad3f
duxiuxing <duxiuxing@foxmail.com>
2024-05-24 01:42:57 +0800

    code review: check_rom_file_names.py

--- python/check_rom_file_names.py
+++ python/check_rom_file_names.py
@@ -1,7 +1,4 @@
-#-- coding: UTF-8 --
-# 检查 roms\\cps1 文件夹里面 ROM 文件的名称，是否和 wiiflow\\plugins_data\\CPS1\\CPS1.ini 中的文件名称一致
-# 检查 roms\\cps2 文件夹里面 ROM 文件的名称，是否和 wiiflow\\plugins_data\\CPS2\\CPS2.ini 中的文件名称一致
-# 检查 roms\\cps3 文件夹里面 ROM 文件的名称，是否和 wiiflow\\plugins_data\\CPS3\\CPS3.ini 中的文件名称一致
+# -- coding: UTF-8 --
 
 import os
 import zlib
@@ -19,7 +16,8 @@ def compute_crc32(file_path):
 
 def check_roms_is_in_ini_file(roms_relative_dir, ini_relative_file_path, section_name):
     config_parser = ConfigParser()
-    ini_file_path = os.path.join(LocalConfigs.REPOSITORY_FOLDER, ini_relative_file_path)
+    ini_file_path = os.path.join(
+        LocalConfigs.REPOSITORY_FOLDER, ini_relative_file_path)
     config_parser.read(ini_file_path)
 
     roms_dir = os.path.join(LocalConfigs.REPOSITORY_FOLDER, roms_relative_dir)
@@ -35,7 +33,7 @@ def check_roms_is_in_ini_file(roms_relative_dir, ini_relative_file_path, section
             option = str(item_name)[:-4]
             if config_parser.has_option(section_name, option) is True:
                 continue
-            
+
             crc32 = compute_crc32(item_path)
             option = str(item_name)[:-5]
             if config_parser.has_option(section_name, option) is True:
@@ -50,9 +48,10 @@ def check_roms_is_in_ini_file(roms_relative_dir, ini_relative_file_path, section
     print(f"List Directory End: {roms_dir}")
 
 
-def check_roms_in_ini_file_is_exist(roms_relative_dir, ini_relative_file_path, section_name, exclude_names = None):
-    ini_file_path = os.path.join(LocalConfigs.REPOSITORY_FOLDER, ini_relative_file_path)
-    print(f"Check Ini Start: {ini_file_path}")
+def check_roms_in_ini_file_is_exist(roms_relative_dir, ini_relative_file_path, section_name, exclude_names=None):
+    ini_file_path = os.path.join(
+        LocalConfigs.REPOSITORY_FOLDER, ini_relative_file_path)
+    print(f"Check ini Start: {ini_file_path}")
 
     config_parser = ConfigParser()
     config_parser.read(ini_file_path)
@@ -73,14 +72,23 @@ def check_roms_in_ini_file_is_exist(roms_relative_dir, ini_relative_file_path, s
         print("ERROR: file name not match")
         print(f"- name: {key}")
 
-    print(f"Check Ini End: {ini_file_path}")
+    print(f"Check ini End: {ini_file_path}")
 
 
-check_roms_is_in_ini_file("roms\\cps1", "wiiflow\\plugins_data\\CPS1\\CPS1.ini", "CPS1")
-check_roms_in_ini_file_is_exist("roms\\cps1", "wiiflow\\plugins_data\\CPS1\\CPS1.ini", "CPS1", ("cps1frog", "ganbare", "pokonyan"))
+# 检查 roms\\cps1 文件夹里面 ROM 文件，是否和 CPS1.ini 中的文件名称一致
+check_roms_is_in_ini_file(
+    "roms\\cps1", "wiiflow\\plugins_data\\CPS1\\CPS1.ini", "CPS1")
+check_roms_in_ini_file_is_exist(
+    "roms\\cps1", "wiiflow\\plugins_data\\CPS1\\CPS1.ini", "CPS1", ("cps1frog", "ganbare", "pokonyan"))
 
-check_roms_is_in_ini_file("roms\\cps2", "wiiflow\\plugins_data\\CPS2\\CPS2.ini", "CPS2")
-check_roms_in_ini_file_is_exist("roms\\cps2", "wiiflow\\plugins_data\\CPS2\\CPS2.ini", "CPS2")
+# 检查 roms\\cps2 文件夹里面 ROM 文件，是否和 CPS2.ini 中的文件名称一致
+check_roms_is_in_ini_file(
+    "roms\\cps2", "wiiflow\\plugins_data\\CPS2\\CPS2.ini", "CPS2")
+check_roms_in_ini_file_is_exist(
+    "roms\\cps2", "wiiflow\\plugins_data\\CPS2\\CPS2.ini", "CPS2")
 
-check_roms_is_in_ini_file("roms\\cps3", "wiiflow\\plugins_data\\CPS3\\CPS3.ini", "CPS3")
-check_roms_in_ini_file_is_exist("roms\\cps3", "wiiflow\\plugins_data\\CPS3\\CPS3.ini", "CPS3")
+# 检查 roms\\cps3 文件夹里面 ROM 文件，是否和 CPS3.ini 中的文件名称一致
+check_roms_is_in_ini_file(
+    "roms\\cps3", "wiiflow\\plugins_data\\CPS3\\CPS3.ini", "CPS3")
+check_roms_in_ini_file_is_exist(
+    "roms\\cps3", "wiiflow\\plugins_data\\CPS3\\CPS3.ini", "CPS3")
